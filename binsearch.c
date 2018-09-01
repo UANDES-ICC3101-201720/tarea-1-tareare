20cc20321c675c62a02fd139f20916c9ae7fd1c7
barracius <rjgonzalez@miuandes.cl>
2018-08-31 22:55:59 -0300

    Avance 1.3

--- binsearch.c
+++ binsearch.c
@@ -1,8 +1,10 @@
-#define _POSIX_C_SOURCE 2
+#define _POSIX_C_SOURCE 199309L
 #include <ctype.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
+#include <inttypes.h>
+#include <pthread.h>
 #include <unistd.h>
 #include <sys/socket.h>
 #include <sys/un.h>
@@ -10,48 +12,130 @@
 #include <string.h>
 #include <time.h>
 #include <unistd.h>
+#include <sys/wait.h>
+#include <limits.h>
 #include "types.h"
 #include "const.h"
 #include "util.h"
 
-// TODO: implement
-int serial_binsearch(int x,int v[],int n) {
-	int low,high,mid;
-    low=0;
-    high=n-1;
-    while(low<high)
-    {
-        mid = ( low + high ) / 2;
+pthread_mutex_t region_mutex = PTHREAD_MUTEX_INITIALIZER;
 
-        if( x < v[mid])
-            high  = mid - 1;
-        else if ( x > v[mid])
-            low = mid + 1;
-        else
-            return mid;
-    }
-    return -1;
+struct final{
+	int x;
+	int y;
+	int z[];
+};
+
+int serial_binsearch(int A[], int n, int T) {
+	int L = 0;
+	int R = n-1;
+	int m;
+	while (L <= R){
+	    	m = ((L + R) / 2);
+		if (A[m] < T)
+		    L = m + 1;
+		else if (A[m] > T)
+		    R = m - 1;
+		else		
+		    return m;
+	}
+	return -1;    
 }
 
-// TODO: implement
-int parallel_binsearch() {
-    return 0;
+void *first(void * fv){
+	struct final *fs = fv;
+	pthread_exit((void *) (intptr_t) serial_binsearch(fs->z, fs->x, fs->y)); 
 }
- 
+void *second(void * fv){
+	struct final *fs = fv;
+	pthread_exit((void *) (intptr_t) serial_binsearch(fs->z, fs->x, fs->y)); 
+}
+void *third(void * fv){
+	struct final *fs = fv;
+	pthread_exit((void *) (intptr_t) serial_binsearch(fs->z, fs->x, fs->y)); 
+}
+void *fourth(void * fv){
+	struct final *fs = fv;
+	pthread_exit((void *) (intptr_t) serial_binsearch(fs->z, fs->x, fs->y));
+}
+
+int parallel_binsearch(int A[], int n, int T) {
+	int B[n/3];
+	
+	int C[n/3];
+	
+	int D[n/3];
+
+	for(int i=0; i<n/3; i++){B[i] = A[i]; } 
+	for(int i=n/3; i<2*(n/3); i++){C[i-(n/3)] = A[i]; } 
+	for(int i=2*(n/3); i<n; i++){D[i-(2*n/3)] = A[i]; } 	
+
+	struct final *fi = malloc(sizeof(struct final)+(n/3)*sizeof(int));
+	fi->x = n/3;
+	fi->y = A[T];
+	memcpy(fi->z, B, (n/3)*sizeof(int));
+	
+	struct final *se = malloc(sizeof(struct final)+(n/3)*sizeof(int));
+	se->x = n/3;
+	se->y = A[T];
+	memcpy(se->z, C, (n/3)*sizeof(int));	
+
+	struct final *th = malloc(sizeof(struct final)+(n/3)*sizeof(int));
+	th->x = n/3;
+	th->y = A[T];
+	memcpy(th->z, D, (n/3)*sizeof(int));
+
+	struct final *fo = malloc(sizeof(struct final)+(n/3)*sizeof(int));
+	fo->x = n/3;
+	fo->y = A[T];
+	memcpy(fo->z, D, (n/3)*sizeof(int));
+
+	pthread_t first_thread;
+	pthread_t second_thread;
+	pthread_t third_thread;
+	pthread_t fourth_thread;
+	
+	pthread_create(&first_thread, NULL, first, fi);
+	void * fv;
+	pthread_join(first_thread, &fv);
+	int q = (intptr_t) fv;	
+
+	pthread_create(&second_thread, NULL, second, se);
+	void * fq;
+	pthread_join(second_thread, &fq);
+	int w = (intptr_t) fq;
+
+	pthread_create(&third_thread, NULL, third, th);
+	void * fr;
+	pthread_join(third_thread, &fr);
+	int f = (intptr_t) fr;	
+	
+	pthread_create(&fourth_thread, NULL, fourth, fo);
+	void * ft;
+	pthread_join(fourth_thread, &ft);
+	int p = (intptr_t) ft;	
+
+	if(q >= 0){return q;}
+	else if(w >= 0){return (w+333);}
+	else if(f >= 0){return (f+666);}
+	else if(p >= 0){return (p+999);}
+	return -1;
+}
+
 int main(int argc, char** argv) {
+	/* TODO: move this time measurement to right before the execution of each binsearch algorithms
+	* in your experiment code. It now stands here just for demonstrating time measurement. */
+	//clock_t cbegin = clock();
+	
+	printf("[binsearch] Starting up...\n");
 
-    /* TODO: move this time measurement to right before the execution of each binsearch algorithms
-     * in your experiment code. It now stands here just for demonstrating time measurement. */
-    clock_t cbegin = clock();
+	/* Get the number of CPU cores available */
+	printf("[binsearch] Number of cores available: '%ld'\n",
+	   sysconf(_SC_NPROCESSORS_ONLN));
 
-    printf("[binsearch] Starting up...\n");
+	/* parse arguments with getopt */
 
-    /* Get the number of CPU cores available */
-    printf("[binsearch] Number of cores available: '%ld'\n",
-           sysconf(_SC_NPROCESSORS_ONLN));
-
-    /* TODO: parse arguments with g etopt */
-    int Tvalue = 0;
+	char* Tvalue;
 	int Evalue = 0;
 	int Pvalue = 0;
 	int index;
@@ -66,7 +150,7 @@ int main(int argc, char** argv) {
 				Evalue = atoi(optarg);
 				break;
 			case 'T':
-				Tvalue = atoi(optarg);
+				Tvalue = optarg;
 				break;
 			case 'P':
 				Pvalue = atoi(optarg);
@@ -86,14 +170,14 @@ int main(int argc, char** argv) {
 		}
 
 
-	printf ("E = %d, T = %d, P = %d\n",
+	printf ("E = %d, T = %s, P = %d\n",
 	  Evalue, Tvalue, Pvalue);
 
 	for (index = optind; index < argc; index++)
 		printf ("Non-option argument %s\n", argv[index]);
-    
 
-    /* TODO: start datagen here as a child process. */
+	/* start datagen here as a child process. */
+
 	pid_t pid;
 	pid = fork();
 	if (pid == -1){   
@@ -101,31 +185,99 @@ int main(int argc, char** argv) {
 		exit(EXIT_FAILURE);
 	}
 	if (pid == 0){
-		printf("%s\n", "Soy el hijo" );
 		execlp("./datagen","./datagen",NULL);
 	}
-	else{
-		printf("%s\n", "Soy el padre" );
-    }
+	//else {wait(NULL); }
 
-    /* TODO: implement code for your experiments using data provided by datagen and your
-     * serial and parallel versions of binsearch.
-     * */
+	/* connect to datagen and ask for the necessary data in each experiment round.
+	* Create a Unix domain socket with DSOCKET_PATH (see const.h).
+	* Talk to datagen using the messages specified in the assignment description document.
+	* Read the values generated by datagen from the socket and use them to run your
+	* experiments
+	* */
 
-    /* TODO: connect to datagen and ask for the necessary data in each experiment round.
-     * Create a Unix domain socket with DSOCKET_PATH (see const.h).
-     * Talk to datagen using the messages specified in the assignment description document.
-     * Read the values generated by datagen from the socket and use them to run your
-     * experiments
-     * */
+	struct sockaddr_un addr;
+  	int fd,rc;
+	char buf[10];
+	strcpy(buf,"BEGIN S");
+	strcat(buf, Tvalue);
+	
+	if ( (fd = socket(AF_UNIX, SOCK_STREAM, 0)) == -1) {
+		perror("socket error");
+		exit(-1);
+	}
 
-    /* Probe time elapsed. */
-    clock_t cend = clock();
+	memset(&addr, 0, sizeof(addr));
+	addr.sun_family = AF_UNIX;
+	strncpy(addr.sun_path, DSOCKET_PATH, sizeof(addr.sun_path)-1);
 
-    // Time elapsed in miliseconds.
-    double time_elapsed = ((double) (cend - cbegin) / CLOCKS_PER_SEC) * 1000;
+	if (connect(fd, (struct sockaddr*)&addr, sizeof(addr)) == -1) {
+		perror("connect error");
+		exit(-1);
+	}
+	//Sending T flag:
+	if ((rc = write(fd, buf, sizeof(buf))) == -1){
+		perror("write error\n");
+		exit(-1);
+	}
+	printf("Enviando info: %d bytes enviados.\n", rc);
 
-    printf("Time elapsed '%lf' [ms].\n", time_elapsed);
+	//Reading data and saving data:
+	int buf2[1000];
+	int linea=0;
+	int len=pow(10,atoi(Tvalue));
+	int arreglo[len];
+	while ((rc = read(fd, buf2, sizeof(buf2))) >= 1000){
+		if (linea==0){
+			for (int i=0;i<1000;i++){
+				arreglo[i+linea*1000]=buf2[i+1];
+			}
+		}
+		else{
+			for (int i=0;i<1000;i++){
+				arreglo[i+linea*1000]=buf2[i];
+			}
+		}
+		linea++;
+	}
+	//Ending datagen process:
+	if ((rc = write(fd, "END", sizeof(buf))) == -1){
+		perror("write error\n");
+		exit(-1);
+	}
+	clock_t cbegin = clock();
+	/* TODO: implement code for your experiments using data provided by datagen and your
+	* serial and parallel versions of binsearch.
+	* */
 
-    exit(0);
+	struct timespec start, finish;
+	double elapsed_s = 0;
+	double elapsed_p = 0;
+	printf("E,T,TIEMPO SERIAL,TIEMPO PARALELO\n");
+	//int av_serial;
+	for (int i=0; i<Evalue; i++){
+
+		clock_gettime(CLOCK_MONOTONIC, &start);
+		serial_binsearch(arreglo,len, arreglo[Pvalue]);
+		clock_gettime(CLOCK_MONOTONIC, &finish);
+		elapsed_s = (finish.tv_sec - start.tv_sec);
+		elapsed_s += (finish.tv_nsec - start.tv_nsec) / 1000000000.0;
+
+		clock_gettime(CLOCK_MONOTONIC, &start);
+		parallel_binsearch(arreglo, len, Pvalue);
+		clock_gettime(CLOCK_MONOTONIC, &finish);
+		elapsed_p = (finish.tv_sec - start.tv_sec);
+		elapsed_p += (finish.tv_nsec - start.tv_nsec) / 1000000000.0;
+		printf("%d,%d,%lf,%lf\n", i+1, atoi(Tvalue), elapsed_s, elapsed_p);	
+	}
+
+	/* Probe time elapsed. */
+	clock_t cend = clock();
+
+	// Time elapsed in miliseconds.
+	double time_elapsed = ((double) (cend - cbegin) / CLOCKS_PER_SEC) * 1000;
+
+	printf("Time elapsed '%lf' [ms].\n", time_elapsed);
+
+	exit(0);
 }
